6e559796f471040ec9aa0faabe37e40bb039c240
Hery Robles <Hery Robles@HERYROBLES>
2022-06-23 22:13:24 -0500

    Grafo

--- Grafo/Program.cs
+++ Grafo/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grafo;
+
+int nodo = 0;
+
+Grafo grafos = new Grafos(6);
+
+//grafos.Arista(1, 2);
+//grafos.Arista(1, 3);
+//grafos.Arista(2, 4);
+//grafos.Arista(3, 5);
+//grafos.Arista(4, 3);
+//grafos.Arista(5, 1);
+//grafos.Arista(5, 4);
+
+//grafos.MuestraAdyacencia();
